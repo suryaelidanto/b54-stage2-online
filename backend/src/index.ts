dba780c1589a75f146a671061c2fb3054d42fd39
suryaelidanto <suryaelidanto@gmail.com>
2024-06-10 10:38:42 +0700

    chore(backend): fixing for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ import { authenticate } from "./middlewares/authenticate";
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const router = express.Router();
 const routerv2 = express.Router();
 
