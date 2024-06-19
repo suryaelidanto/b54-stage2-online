9660974c909ffb1828c086be6ae260a466b7dd48
aristu09 <namikazeuzumaki43@gmail.com>
2024-06-19 11:26:47 +0700

    update(style-FollowPage)

---
+++ frontend/src/pages/follows.tsx
@@ -0,0 +1,12 @@
+import { FollowCard } from "@/features/follows/component/follow-card";
+import { Box } from "@chakra-ui/react";
+
+export function FollowPage() {
+  return (
+    <>
+      <Box>
+        <FollowCard />
+      </Box>
+    </>
+  );
+}
